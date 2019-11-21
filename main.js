004de515d69ab39c832e2e580ce2eff7ebf50076
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-21 14:06:31 -0500

    finished

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const nasafy = function(num) {
   let currentNum = num
   while (currentNum > 0){
 arr.push(currentNum);
+currentNum --
   }
   arr.push("Blast off!");
   return arr;
@@ -18,11 +19,29 @@ const reversify = function(str) {
 
 
 const titleify = function(str) {
-  
+  const arr = [];
+  for (const word of str.split(' ')) {
+    arr.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+  }
+
+  return arr.join(' ');
 }
 
+
 const crazify = function(str) {
+  let newStr = '';
+  let i = 0;
   
+  for (const letter of str) {
+    if (letter === ' ') {
+      newStr += ' ';
+    } else {
+      newStr += i % 2 === 1 ? letter.toUpperCase() : letter.toLowerCase();
+     i++;
+    }
+  }
+
+  return newStr;
 }
 
 
