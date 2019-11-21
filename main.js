e18c02cd058da5caaa1590cf3da16f7bbc10079a
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-21 13:48:45 -0500

    half way

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const biggify = function(num) {
-  
+  return num + 9000;
 }
 
 const nasafy = function(num) {
-  
+  const arr = [];
+  let currentNum = num
+  while (currentNum > 0){
+arr.push(currentNum);
+  }
+  arr.push("Blast off!");
+  return arr;
 }
 
 const reversify = function(str) {
-  
-}
+  return str.split('').reverse().join('');
+  }
+
 
 const titleify = function(str) {
   
